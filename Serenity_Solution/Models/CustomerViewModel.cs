8f29ee0341f1c070dfd9a8d007c14f76c2f1b148
thienlcde170520 <thienle2105@gmail.com>
2025-05-22 23:10:09 +0700

    UI profile + Edit

--- Serenity_Solution/Models/CustomerViewModel.cs
+++ Serenity_Solution/Models/CustomerViewModel.cs
@@ -5,12 +5,10 @@
         public string Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
+        public string Phone { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public string Gender { get; set; }
         public string Address { get; set; }
-        public string ProfilePictureUrl { get; set; }
-        public int LoyaltyPoints { get; set; }
-        public string MembershipType { get; set; }
-        public DateTime JoinDate { get; set; }
+        public string? ProfilePictureUrl { get; set; }
     }
 }
