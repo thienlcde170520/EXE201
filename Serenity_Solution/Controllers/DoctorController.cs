c202d90440cba35f4596b2e563a9247df07cec9d
thienlcde170520 <thienle2105@gmail.com>
2025-05-25 18:01:46 +0700

    Update Data+Booking

--- Serenity_Solution/Controllers/DoctorController.cs
+++ Serenity_Solution/Controllers/DoctorController.cs
@@ -2,8 +2,13 @@
 using EXE201.Commons.Data;
 using EXE201.Commons.Models;
 using EXE201.Services.Interfaces;
+using EXE201.Services.Models;
+using EXE201.Services.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Serenity_Solution.Models;
+using System.Threading.Tasks;
 
 namespace Serenity_Solution.Controllers
 {
@@ -15,8 +20,9 @@ namespace Serenity_Solution.Controllers
         private readonly SignInManager<User> _signInManager;
         private readonly ApplicationDbContext _context;
         private readonly IEmailService _emailService;
+        private readonly IVnPayServicecs _vpnPayServicecs;
         public DoctorController(UserManager<User> userManager, IAccountService accountService, IEmailService emailService, SignInManager<User> signInManager,
-            ApplicationDbContext context, Cloudinary cloudinary)
+            ApplicationDbContext context, Cloudinary cloudinary, IVnPayServicecs vnPayServicecs)
         {
             _cloudinary = cloudinary;
             _accountService = accountService;
@@ -24,8 +30,9 @@ namespace Serenity_Solution.Controllers
             _emailService = emailService;
             _signInManager = signInManager;
             _context = context;
+            _vpnPayServicecs = vnPayServicecs;
         }
-        public async Task<IActionResult> Index(int page = 1, int pageSize = 3)
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 2)
         {
             var users = await _userManager.GetUsersInRoleAsync("Psychologist");
 
@@ -36,14 +43,154 @@ namespace Serenity_Solution.Controllers
             if (doctors.Count == 0)
             {
                 TempData["NoWSDetail"] = true;
-                return RedirectToAction("Index","Home");
+                return RedirectToAction("Index", "Home");
+            }
+            var DoctorList = doctors
+                 .Select(s => new PsychologistViewModel
+                 {
+                     Id = s.Id,
+                     Name = s.Name,
+                     Address = s.Address,
+                     Price = s.Price,
+                     Description = s.Description,
+                     Experience = s.Experience,
+                     ProfilePictureUrl = s.ProfilePictureUrl,
+                 })
+                 .ToList();
+            int totalUsers = DoctorList.Count();
+            var pagedUsers = DoctorList.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalUsers / pageSize);
+            ViewBag.CurrentPage = page;
+            // Gửi danh sách Staff kèm ID (dùng ViewBag nếu cần)
+            ViewBag.StaffIds = doctors.ToDictionary(s => s.Email, s => s.Id);
+
+            return View(pagedUsers);
+
+
+        }
+        public async Task<IActionResult> Detail(string Id)
+        {
+            var user = await _userManager.FindByIdAsync(Id);
+            if (user == null) { return NotFound(); }
+            var psychologist = new PsychologistViewModel
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                Phone = user.Phone,
+                Address = user.Address,
+                Degree = user.CertificateUrl,
+                Description = user.Description,
+                Experience = user.Experience,
+                Price = user.Price,
+                ProfilePictureUrl = user.ProfilePictureUrl,// hoặc null để dùng ảnh mặc định
+                Major = user.Major
+            };
+
+            return View(psychologist);
+        }
+
+        [Authorize]
+        public IActionResult Payment(string Id)
+        {
+            if (string.IsNullOrEmpty(Id))
+                return BadRequest("Doctor ID is required");
+
+            var doctorSelected = _userManager.FindByIdAsync(Id).Result;
+            if (doctorSelected == null) { return NotFound(); }
+
+            var userBooking = _userManager.GetUserAsync(User).Result;
+            if (userBooking == null)
+                return Unauthorized("User not logged in");
+
+            var orderId = new Random().Next(1000, 100000);
+            var vnPayModel = new VnPaymentRequestModel
+            {
+                Amount = (double)doctorSelected.Price,
+                CreateDate = DateTime.Now,
+                Description = $"Thanh toán tư vấn bác sĩ {doctorSelected.Name}",
+                FullName = userBooking.Name,
+                OrderId = orderId,
+                Psychologist = doctorSelected
+            };
+            return Redirect(_vpnPayServicecs.CreatePaymentUrl(HttpContext, vnPayModel));
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> PaymentCallBackAsync()
+        {
+            try
+            {
+                var response = _vpnPayServicecs.PaymentExecute(Request.Query);
+                var code = response.VnPayResponseCode;
+                var orderInfo = response.OrderDescription;
+                var doctorId = (orderInfo);
+                if (string.IsNullOrEmpty(doctorId))
+                {
+                    TempData["error"] = "Không tìm thấy thông tin bác sĩ";
+                    return RedirectToAction("Index", "Doctor");
+                }
+                var doctorSelected = _userManager.FindByIdAsync(doctorId).Result;
+                if (response == null || response.VnPayResponseCode != "00")
+                {
+                    TempData["error"] = "Lỗi thanh toán";
+                    return RedirectToAction("Detail", "Doctor", new { Id = doctorId });
+
+                }
+                var currentUser = await _userManager.GetUserAsync(User);
+
+
+                Appointment invoice = new Appointment();
+                invoice.Psychologist_ID = doctorSelected.Id;
+                invoice.Client_ID = currentUser.Id;
+                invoice.Price = doctorSelected.Price;
+                invoice.Status = "Booked";
+                invoice.Scheduled_time = DateTime.Now.AddDays(3);
+                invoice.Created_at = DateTime.Now;
+                invoice.Notes = "Thanh toán thành công qua VNPay";
+
+                //await _context.AddAsync(invoice);
+                await _context.Appointments.AddAsync(invoice);
+                
+
+                var customer = await _userManager.FindByIdAsync(invoice.Client_ID);
+
+                if (doctorSelected != null)
+                {
+                    await _emailService.SendEmailAsync(doctorSelected.Email, "Đặt lịch hẹn thành công", $"Bạn đã được đặt lịch tư vấn với {customer.Name}. Vui lòng kiểm tra thông tin chi tiết trong hệ thống.");
+                }
+                doctorSelected.BaBalance  += (double)doctorSelected.Price * 0.9;
+                await _userManager.UpdateAsync(doctorSelected);
+                await _context.SaveChangesAsync();
+
+                TempData["success"] = "Thanh toán thành công";
+
+                return RedirectToAction("Detail", "Doctor", new { Id = doctorId });
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Có lỗi xảy ra trong quá trình xử lý";
+                return RedirectToAction("Index", "Doctor");
             }
 
-            return View();
         }
-        public IActionResult Details()
+        private string ExtractDoctorIdFromOrderInfo(string orderInfo)
         {
-            return View();
+            if (string.IsNullOrEmpty(orderInfo))
+                return null;
+
+            try
+            {
+                var parts = orderInfo.Split('|');
+                var doctorPart = parts.FirstOrDefault(p => p.StartsWith("DOCTOR_ID:"));
+                return doctorPart?.Substring("DOCTOR_ID:".Length);
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
--- Serenity_Solution/Seeders/IdentitySeeder.cs
+++ Serenity_Solution/Seeders/IdentitySeeder.cs
@@ -38,5 +38,74 @@ namespace Serenity_Solution.Seeders
                 }
             }
         }
+        //add psychologist
+        public static async Task SeedDataPsychologist(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            // Tạo role nếu chưa có
+            /*
+            string[] roles = new[] { "Psychologist", "Customer", "Admin" };
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new ApplicationRole(role));
+                }
+            }*/
+
+            // Lê Văn Thắng
+            if (await userManager.FindByEmailAsync("Thang152@gamil.com") == null)
+            {
+                var user = new Psychologist
+                {
+                    UserName = "Lê Văn Thắng",
+                    Email = "Thang152@gamil.com",
+                    Name = "Lê Văn Thắng",
+                    Phone = "123456789",
+                    Major = "Tâm lý học trẻ em",
+                    Address = "HCM, Việt Nam",
+                    Degree = "~image/Degree/cunhantamly.jpg",
+                    Description = "Nhà tâm lý học có nhiều năm kinh nghiệm trong ngành.",
+                    Experience = "10 years",
+                    Price = 1000000,
+                    ProfilePictureUrl = "~image/Doctor/Van_Thang.png",
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(user, "Default@123");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Psychologist");
+                }
+            }
+            //Kim nguyen 
+            if (await userManager.FindByEmailAsync("KimNguyen2105@gamil.com") == null)
+            {
+                var user = new Psychologist
+                {
+                    UserName = "Kim Nguyễn",
+                    Email = "KimNguyen2105@gamil.com",
+                    Name = "Kim Nguyễn",
+                    Phone = "0933555777",
+                    Major = "Tâm lý học trẻ em",
+                    Address = "Cần Thơ, Việt Nam",
+                    Degree = "~image/Degree/cunhantamly.jpg",
+                    Description = "Tư vấn tâm lý cho trẻ em và thanh thiếu niên.",
+                    Experience = "6 years",
+                    Price = 750000,
+                    ProfilePictureUrl = "~image/Doctor/Kim_Nguan.png",
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(user, "Default@123");
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Psychologist");
+                }
+            }
+        }
+
     }
 }
